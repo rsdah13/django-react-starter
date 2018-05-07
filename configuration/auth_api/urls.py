a1a6a50e3cc3bbf70027504028c593b70f42e2d1
Rory Dahl <rsdah13@gmail.com>
2018-05-06 23:04:33 -0700

    Create backend API.

---
+++ configuration/auth_api/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+from rest_framework import routers
+
+from .api import RegistrationAPI, LoginAPI, UserAPI
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    url("^", include(router.urls)),
+    url("^auth/register/$", RegistrationAPI.as_view()),
+    url("^auth/login/$", LoginAPI.as_view()),
+    url("^auth/user/$", UserAPI.as_view()),
+]
